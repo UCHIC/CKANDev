3ae220aa4f4ea881bb99d45c2db4fb5b0fbc2b64
pabitra <pabitra.dash@usu.edu>
2014-04-01 11:08:55 -0600

    User now have to agree to both data publisher's agreement and data user agreement in order to create an account (Issue#40)

--- src/ckanext-Metadata/ckanext/Metadata/public/scripts/user_registration.js
+++ src/ckanext-Metadata/ckanext/Metadata/public/scripts/user_registration.js
@@ -3,15 +3,92 @@
  */
 $(document).ready(function(){
 
-    $('#btnCreateAccount').attr('disabled', true);
+    var dataUseAgree = false;
+    var dataPublishAgree = false;
+
+    var error = $("#field-error").val();
+    if (error != "{}")
+    {
+          if(sessionStorage.length >1)
+        {
+            dataUseAgree = sessionStorage.getItem('dataUseAgree');
+            dataPublishAgree = sessionStorage.getItem('dataPublishAgree');
+        }
+        else
+        {
+            dataUseAgree = false;
+            dataPublishAgree = false;
+        }
+    }
+
+    if (dataPublishAgree && dataUseAgree){
+        $('#chkDataPublishAgreement').prop('checked', true);
+        $('#chkDataUseAgreement').prop('checked', true);
+        $('#btnCreateAccount').attr('disabled', false);
+    }
+    else{
+         $('#btnCreateAccount').attr('disabled', true);
+         $('#chkDataPublishAgreement').prop('checked', false);
+         $('#chkDataUseAgreement').prop('checked', false);
+    }
+
+    $('#btnDataPublishAgree').click(function(){
+        dataPublishAgree = true;
+        sessionStorage.setItem('dataPublishAgree', dataPublishAgree);
+        if (dataUseAgree){
+            $('#btnCreateAccount').attr('disabled', false);
+        }
+    });
+
+    $('#btnDataUseAgree').click(function(){
+        dataUseAgree = true;
+        sessionStorage.setItem('dataUseAgree', dataUseAgree)
+        if (dataPublishAgree){
+            $('#btnCreateAccount').attr('disabled', false);
+        }
+    });
+
+    $('#btnDataPublishDecline').click(function(){
+        dataPublishAgree = false;
+        $('#btnCreateAccount').attr('disabled', true);
+        $('#chkDataPublishAgreement').prop('checked', false);
+        sessionStorage.setItem('dataPublishAgree', dataPublishAgree);
+    });
+
+    $('#btnDataUseDecline').click(function(){
+        dataUseAgree = false;
+        $('#btnCreateAccount').attr('disabled', true);
+        $('#chkDataUseAgreement').prop('checked', false);
+        sessionStorage.setItem('dataUseAgree', dataUseAgree);
+    });
 
     $('#chkDataPublishAgreement').click(function(){
         if($(this).is(':checked')){
-             $('#btnCreateAccount').attr('disabled', false);
-
+            showDataPublishAgreementDialog();
         }
         else{
-              $('#btnCreateAccount').attr('disabled', true)
+            $('#btnCreateAccount').attr('disabled', true);
+             dataPublishAgree = false;
+             sessionStorage.setItem('dataPublishAgree', dataPublishAgree);
+        }
+
+//        if($(this).is(':checked')){
+//             $('#btnCreateAccount').attr('disabled', false);
+//
+//        }
+//        else{
+//              $('#btnCreateAccount').attr('disabled', true)
+//        }
+    });
+
+    $('#chkDataUseAgreement').click(function(){
+         if($(this).is(':checked')){
+            showDataUseAgreementDialog();
+         }
+         else{
+            $('#btnCreateAccount').attr('disabled', true);
+             dataUseAgree = false;
+             sessionStorage.setItem('dataUseAgree', dataUseAgree);
         }
     });
 });
@@ -20,4 +97,10 @@ function showDataPublishAgreementDialog()
 	{
 		$('#showDataPublishModal').css('z-index', 1050);
 		$('#showDataPublishModal').modal('show');
+	}
+
+function showDataUseAgreementDialog()
+	{
+		$('#showDataUseModal').css('z-index', 1050);
+		$('#showDataUseModal').modal('show');
 	}
