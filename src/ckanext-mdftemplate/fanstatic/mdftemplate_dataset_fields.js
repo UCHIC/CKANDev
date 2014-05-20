a13ed02909c91e40717e2910a293d08788d29238
pabitra <pabitra.dash@usu.edu>
2014-05-20 11:17:32 -0600

    Fix for issue#51

--- src/ckanext-mdftemplate/fanstatic/mdftemplate_dataset_fields.js
+++ src/ckanext-mdftemplate/fanstatic/mdftemplate_dataset_fields.js
@@ -3,18 +3,21 @@ $(document).ready(function() {
 	var privacyBubble = $('<span id="privateBubble" title="Datasets are private and not visible to the public until a moderator has reviewed and approved them." class="info-block info-inline" style="cursor:pointer"> ' +
 	        	'<i class="icon-question-sign"></i>' +
 	        	'Why is my dataset private?</span>');
-	
-	var updatePrivacyDropdown = function() {
+
+    var updatePrivacyDropdown = function() {
 		if ($('#field-organizations > option:selected').text() === 'iutah') {
-	    	$('#field-private').val("True");
-	        $('#field-private option[value="False"]').attr('disabled', true);
-	        privacyBubble.insertAfter($('#field-private'));
-	    } else {
-	    	$('#field-private option[value="False"]').attr('disabled', false);
-	    	privacyBubble.remove();
-	    }
+	    	if ($('#field-private option:selected').val() === "True"){
+                $('#field-private option[value="False"]').attr('disabled', true);
+                $('#field-private option[value="True"]').attr('disabled', false);
+                privacyBubble.insertAfter($('#field-private'));
+            } else {
+                $('#field-private option[value="False"]').attr('disabled', false);
+                $('#field-private option[value="True"]').attr('disabled', true);
+                privacyBubble.remove();
+	        }
+        }
 	};
-	
+
 	updatePrivacyDropdown();
 	$('#field-organizations').change(updatePrivacyDropdown);
 	
